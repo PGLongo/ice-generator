fe25aef547bbe2607e8e84e54761c0e0c1fab510
Piergiuseppe Longo <plongo@alteanet.it>
2025-10-16 15:12:43 +0200

    refactor: use ULocaleSelect component from Nuxt UI
    Replace custom UPopover implementation with built-in ULocaleSelect component.
    Map i18n locales to the format expected by ULocaleSelect.

--- app/components/LanguageSelect.vue
+++ app/components/LanguageSelect.vue
@@ -1,37 +1,19 @@
 <script setup lang="ts">
 const { locale, locales, setLocale } = useI18n()
 
-const currentLocale = computed(() => {
-  return locales.value.find(l => l.code === locale.value)
-})
-
-const switchLocale = (code: string) => {
-  setLocale(code)
-}
+// Map our locales to the format expected by ULocaleSelect
+const mappedLocales = computed(() =>
+  locales.value.map((loc) => ({
+    code: loc.code,
+    name: loc.name || (loc.code === 'en' ? 'English' : 'Italiano')
+  }))
+)
 </script>
 
 <template>
-  <UPopover>
-    <UButton
-      color="gray"
-      variant="ghost"
-      :label="currentLocale?.code.toUpperCase()"
-      trailing-icon="i-heroicons-chevron-down-20-solid"
-    />
-
-    <template #panel>
-      <div class="p-2 space-y-1">
-        <UButton
-          v-for="loc in locales"
-          :key="loc.code"
-          color="gray"
-          variant="ghost"
-          :label="loc.name"
-          block
-          :class="{ 'bg-gray-100 dark:bg-gray-800': locale === loc.code }"
-          @click="switchLocale(loc.code)"
-        />
-      </div>
-    </template>
-  </UPopover>
+  <ULocaleSelect
+    v-model="locale"
+    :locales="mappedLocales"
+    @update:model-value="setLocale($event)"
+  />
 </template>
